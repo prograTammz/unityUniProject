7656844b0f96f4dbd38ef3c2e28ce46a0c2cf459
Ahmed-Abohegazy <35717011+Ahmed-Abohegazy@users.noreply.github.com>
2018-12-04 13:30:20 +0200

    Camera_Follow

---
+++ uniGame/Assets/Script/Camera_Follow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour {
+
+ public Transform Target;
+    public float cameraSpeed;
+    public float minX, maxX, minY, maxY;
+	
+	void Start () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        if (Target != null)
+        {
+            Vector2 newCamPosition = Vector2.Lerp(transform.position, Target.position, Time.deltaTime * cameraSpeed);
+            float ClampX = Mathf.Clamp(newCamPosition.x, minX, maxX);
+            float ClampY = Mathf.Clamp(newCamPosition.y, minY, maxY);
+            transform.position=new Vector3(ClampX,ClampY,-10f);
+        }
+    }
+	
+	void Update () {
+		
+	}
+}
+
