bb9530d8e32463bd7951d06ff1d177db9d613459
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2018-12-05 22:10:13 +0200

    Added enemyController

---
+++ uniGame/Assets/Script/enemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour {
+
+    public bool isFacingRight = false;
+    public float maxSpeed = 3f;
+    public int damage = 6;
+    //public AudioClip hit1;
+    //public AudioClip hit2;
+
+    public void flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector3(-(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+
+
+            FindObjectOfType<PlayerStats>().takeDamage(damage);
+
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
