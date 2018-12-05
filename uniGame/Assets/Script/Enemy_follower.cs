c8c97672334a778f833b4706a39502cf09897228
Ahmed-Abohegazy <35717011+Ahmed-Abohegazy@users.noreply.github.com>
2018-12-06 00:01:13 +0200

    scripts for (enemy follower ,spawns) and check point script

---
+++ uniGame/Assets/Script/Enemy_follower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_follower : enemyController {
+
+	// Use this for initialization
+	private player_controler player;
+	public float speedtowardplayer;
+	void Start () {
+		player=FindObjectOfType<player_controler>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position=Vector3.MoveTowards (transform.position,player.transform.position,speedtowardplayer*Time.deltaTime);
+	}
+}
